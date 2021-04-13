72e44dc5d0ac30ac369a0b2f5319b370a8bff3a5
NeilSolomon <neilsolomon89@gmail.com>
2021-04-13 14:34:22 -0400

    added login hint to visitor homepage

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -11,6 +11,14 @@ export default class Homepage extends React.Component {
         <h2>
           Hello {this.props.user.username}! Your role is {this.props.user.role}.
         </h2>
+        {this.props.user.role === "visitor" && (
+          <h3>
+            The homepage and menu render differently according to the type of
+            user that is logged in. To test this click Login, enter a username,
+            then click the Login button. For username use "registered",
+            "manager", "clerk", "delivery", or "computer".
+          </h3>
+        )}
         Includes a list of the 3 most popular products and a list of 3 products
         chosen by the manager.
       </div>
