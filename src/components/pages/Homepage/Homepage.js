8fd0aa19d9794d406bbb665203739443cb829c73
NeilSolomon <neilsolomon89@gmail.com>
2021-05-08 09:32:43 -0400

    added api call to homepage

--- src/components/pages/Homepage/Homepage.js
+++ src/components/pages/Homepage/Homepage.js
@@ -7,67 +7,88 @@
 import React from "react";
 import css from "./Homepage.module.css";
 import ProductSmall from "../../ProductSmall";
-
-import cpu from "../../../mockProducts/cpu.png";
-import flashDrive from "../../../mockProducts/flashDrive.png";
-import gpu from "../../../mockProducts/gpu.png";
-import laptop from "../../../mockProducts/laptop.png";
-import mac from "../../../mockProducts/mac.png";
-import sdCard from "../../../mockProducts/sdCard.png";
+import axios from "axios";
+import _ from "lodash";
 
 export default class Homepage extends React.Component {
-  state = {};
+  state = {
+    bestSelling: [],
+    managersPicks: [],
+  };
+
+  componentDidMount = () => {
+    this.getBestSelling();
+    this.getManagersPicks();
+  };
 
   getBestSelling = () => {
-    /**
-     * Make API Call
-     */
-    return [
-      {
-        name: "DDRAM",
-        price: 219.99,
-        company: "AMD",
-        product_id: "111",
-      },
-      {
-        name: "Chromebook",
-        price: 200,
-        company: "Apple",
-        product_id: "555",
-      },
-      {
-        name: "Life Cyclones T-shirt",
-        price: 19.99,
-        company: "Life Cyclones",
-        product_id: "666",
-      },
-    ];
+    axios.defaults.headers = {
+      "X-Parse-Application-Id": process.env.REACT_APP_API_ID,
+      "X-Parse-REST-API-Key": process.env.REACT_APP_API_KEY,
+    };
+    axios
+      .get("https://parseapi.back4app.com/classes/Products")
+      .then((response) => {
+        console.log("getProducts", response.data.results);
+        var products = [];
+        for (const product of response.data.results) {
+          products.push({
+            product_id: product.product_id,
+            object_id: product.objectId,
+            price: product.price,
+            companyObjectId: product.computer_company.objectId,
+            company: "",
+            name: product.product_name,
+            imageSrc: product.Image.url,
+          });
+        }
+
+        const bestSelling = [
+          _.cloneDeep(products[0]),
+          _.cloneDeep(products[1]),
+          _.cloneDeep(products[2]),
+        ];
+
+        this.setState({ bestSelling: bestSelling });
+      })
+      .catch((error) => {
+        console.log("getProducts", error);
+      });
   };
 
   getManagersPicks = () => {
-    /**
-     * Make API Call
-     */
-    return [
-      {
-        name: "GPU",
-        price: 21.99,
-        company: "AMD",
-        product_id: "222",
-      },
-      {
-        name: "Mac Book",
-        price: 2000,
-        company: "Apple",
-        product_id: "333",
-      },
-      {
-        name: "Hard Drive",
-        price: 19.99,
-        company: "Samsung",
-        product_id: "444",
-      },
-    ];
+    axios.defaults.headers = {
+      "X-Parse-Application-Id": process.env.REACT_APP_API_ID,
+      "X-Parse-REST-API-Key": process.env.REACT_APP_API_KEY,
+    };
+    axios
+      .get("https://parseapi.back4app.com/classes/Products")
+      .then((response) => {
+        console.log("getProducts", response.data.results);
+        var products = [];
+        for (const product of response.data.results) {
+          products.push({
+            product_id: product.product_id,
+            object_id: product.objectId,
+            price: product.price,
+            companyObjectId: product.computer_company.objectId,
+            company: "",
+            name: product.product_name,
+            imageSrc: product.Image.url,
+          });
+        }
+
+        const managersPicks = [
+          _.cloneDeep(products[1]),
+          _.cloneDeep(products[2]),
+          _.cloneDeep(products[3]),
+        ];
+
+        this.setState({ managersPicks: managersPicks });
+      })
+      .catch((error) => {
+        console.log("getProducts", error);
+      });
   };
 
   render() {
@@ -81,25 +102,33 @@ export default class Homepage extends React.Component {
         </div>
         <div className={css.productList}>
           <div className={css.subtitle}>Best Selling</div>
-          {this.getBestSelling().map((product) => (
+          {this.state.bestSelling.map((product) => (
             <ProductSmall
+              key={product.object_id}
               name={product.name}
               price={product.price}
-              company={product.company}
-              product_id={product.product_id}
+              company={
+                this.state.companyIdToCompanyName?.[product.companyObjectId]
+              }
+              product_id={product.object_id}
               goToProductPage={this.props.goToProductPage}
+              imageSrc={product.imageSrc}
             />
           ))}
         </div>
         <div className={css.productList}>
           <div className={css.subtitle}>Managers Picks</div>
-          {this.getManagersPicks().map((product) => (
+          {this.state.managersPicks.map((product) => (
             <ProductSmall
+              key={product.object_id}
               name={product.name}
               price={product.price}
-              company={product.company}
-              product_id={product.product_id}
+              company={
+                this.state.companyIdToCompanyName?.[product.companyObjectId]
+              }
+              product_id={product.object_id}
               goToProductPage={this.props.goToProductPage}
+              imageSrc={product.imageSrc}
             />
           ))}
         </div>
